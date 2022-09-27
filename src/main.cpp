c3f383817e63d655a7153a95355234f73736f53b
dylan <dylan@dylan-thinkpad.bham.ac.uk>
2022-09-27 12:08:37 +0100

    improved debugging

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,9 @@
 // }
 
 int main() {
-	Board myBoard = Board();
-	myBoard.loadFEN("3Q2b1/8/R2b2r1/4q2K/4P3/1BkP4/1n6/3N4/ w - - 0 1");
-	myBoard.show();
-	std::cout << myBoard.convertIntToStringMove({6, 4, 4, 4});
-	return 0;
+    Board myBoard = Board();
+    myBoard.loadFEN("3Q2b1/8/R2b2r1/4q2K/4P3/1BkP4/1n6/3N4/ w - - 0 1");
+    myBoard.show();
+    std::cout << myBoard.convertIntToStringMove({6, 4, 4, 4});
+    return 0;
 }
